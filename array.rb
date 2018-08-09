d766440bf73b48c6429583f16f8a6cf5a8cbf614
destinybbrianna <destinybbrianna@gmail.com>
2018-08-09 22:57:15 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,15 @@
+count = 0 
+
+favorite_books = ["catching fire", "a tree grows brooklyn", "to kill a mocking bird", "the book thief" ]
+favorite_books.push("papa johns menu")
+puts favorite_books.size
+
+
+age_of_my_members = [17, 15 , 16, 16 ,16]
+
+
+
+
+
+
+
--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,21 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+
+def invitation
+  guest_name = Hermione 
+  party_name = Best Halloween Party 
+  date = "October 31" 
+time = "6 pm" 
+host_name = Harry Potter 
+ puts  Dear, #{guest_name}
+   You are cordially invited to the #{party_name} ever puts on #{date} at #{time}. No later than #{date}
+
+ Sincerely, 
+ puts  #{host name}
+  
+end 
+invitation 
+
+ 
